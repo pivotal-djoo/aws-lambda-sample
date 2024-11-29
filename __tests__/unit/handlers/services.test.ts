f0dac6eb2cb20f9d0869d6211b763d0bee2fa2fc
Dave Joo <taehyunjoo@gmail.com>
2024-11-29 15:27:24 -0500

    Added available services endpoint; typescript lambda function deployed using sam cli

---
+++ __tests__/unit/handlers/services.test.ts
@@ -0,0 +1,14 @@
+import { LambdaFunctionURLEvent } from 'aws-lambda';
+import getAllServicesEvent from '../../../events/get-all-services.json';
+import { servicesHandler } from '../../../handlers/services';
+
+describe('Test services Handler', () => {
+  it('should get all services', async () => {
+    const result = await servicesHandler(
+      getAllServicesEvent as LambdaFunctionURLEvent
+    );
+
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body!)).toHaveLength(6);
+  });
+});
